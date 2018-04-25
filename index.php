5d99f2f43c1d3d62187615aadb2130d6f7a1f743
Tristan Bennett <tmbennett2011@msn.com>
2018-04-25 14:13:17 -0600

    added a list of things to do

--- index.php
+++ index.php
@@ -10,6 +10,16 @@
   </header>
   <main>
     <p>This is the body of the page. Do not worry about what goes here until you are able to use the information on the flyer given to you.</p>
+    <div>
+
+    </div>
+        <ul>
+          <li>Header</li>
+          <li>Nav Bar(Internal links)</li>
+          <li>Side Bar (External Links)</li>
+          <li>Footer</li>
+        </ul>
+      </div>
   </main>
 </body>
 </html>
