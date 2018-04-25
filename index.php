8ca236d30b709d1a4cd2bbce4ccba99fac6d8e77
Tristan Bennett <tmbennett2011@msn.com>
2018-04-25 13:20:58 -0600

    added header, body, and main tags to page

--- index.php
+++ index.php
@@ -5,6 +5,12 @@
   <title>Bennett Nutrition</title>
 </head>
 <body>
-  <h1>Bennett Nutrition</h1>
+  <header>
+    <h1>Bennett Nutrition</h1>
+  </header>
+  <main>
+    <p>This is the body of the page. Do not worry about what goes here until you are able to use the information on the flyer given to you.</p>
+    
+  </main>
 </body>
 </html>
