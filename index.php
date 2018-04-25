3afa800a10fe6be8032e41f64267441d38aaf773
Tristan Bennett <tmbennett2011@msn.com>
2018-04-25 14:59:30 -0600

    changed list and updated css

--- index.php
+++ index.php
@@ -4,28 +4,26 @@
 <head>
   <meta charset="utf-8" />
 
-<link rel="stylesheet" href="css/splash.css">
+<pnk rel="stylesheet" href="css/splash.css">
 
   <title>Bennett Nutrition</title>
 </head>
 
 <body>
-  <header>
+  <p>
     <h1>Bennett Nutrition</h1>
-  </header>
+  </p>
   <main>
     <p>This is the body of the page. Do not worry about what goes here until you are able to use the information on the flyer given to you.</p>
     <div>
 
     </div>
-        <ul>
-          <li>Bio Section</li>
-          <li>Header</li>
-          <li>Nav Bar(Internal links)</li>
-          <li>Side Bar (External Links)</li>
-          <li>Portfolio Section</li>
-          <li>Footer</li>
-        </ul>
+          <p>Bio Section</p>
+          <p>Header</p>
+          <p>Nav Bar(Internal pnks)</p>
+          <p>Side Bar (External pnks)</p>
+          <p>Portfopo Section</p>
+          <p>Footer</p>
     </div>
   </main>
 </body>
